0a84d65585afaa14c6229d7ae377c11c5d39afea
wquynguy <wquyenle@gmail.com>
2024-02-09 16:09:17 -0600

    aadded styling to header & nav bar, added about contents to home.ejs, and attach routes to index.js

--- Application-Codes/src/index.js
+++ Application-Codes/src/index.js
@@ -7,11 +7,27 @@ const app = express();
 // view engine setup
 app.set('view engine', 'ejs');
 
+// static folder path
+app.use(express.static("public"));
 
 // routes 
 app.get("/", (req, res) => {
     res.render("home");
 });
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
+
+app.get("/products", (req, res) => {
+    res.render("products");
+});
+
+
 
 // establish port for server
 const port = 3030;
