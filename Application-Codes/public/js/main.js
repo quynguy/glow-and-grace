a9470ae9ad749612f66ed1419461cb3469446747
quynguy <wquyenle@gmail.com>
2024-03-31 16:57:53 -0500

    completed styling for cart page

--- Application-Codes/public/js/main.js
+++ Application-Codes/public/js/main.js
@@ -1,24 +1,132 @@
 console.log("js loaded");
 
-const addToCartBtn = document.querySelectorAll('.cart-btn');
 
-function addToCart(event) {
-    event.preventdefault();
+/* Set values + misc */
+var promoCode;
+var promoPrice;
+var fadeTime = 300;
+var expressCost = 12;
+var groundCost = 5;
 
-    const button = event.target;
-    const productId = button.dataset.productId;
-    const productName = button.parentNode.querySelector('.product-name').innerText;
-    const productPrice = button.parentNode.querySelector('.product-price').innerText;
+/* Assign actions */
+$('.quantity input').change(function() {
+  updateQuantity(this);
+});
 
-    console.log('Added to cart:', { id: productId, name: productName, price: productPrice });
+$('.remove button').click(function() {
+  removeItem(this);
+});
 
-}
+$(document).ready(function() {
+  updateSumItems();
+});
 
-addToCartBtn.forEach(button => {
-    button.addEventListener('click', addToCart);
+$('.promo-code-cta').click(function() {
+
+  promoCode = $('#promo-code').val();
+
+  if (promoCode == '10off' || promoCode == '10OFF') {
+    if (!promoPrice) {
+      promoPrice = 10;
+    } else if (promoCode) {
+      promoPrice = promoPrice * 1;
+    }
+  } else if (promoCode != '') {
+    alert("Invalid Promo Code");
+    promoPrice = 0;
+  }
+  
+  if (promoPrice) {
+    $('.summary-promo').removeClass('hide');
+    $('.promo-value').text(promoPrice.toFixed(2));
+    recalculateCart(true);
+  }
 });
 
 
-function productDetails(event) {
-    
+
+
+/* Recalculate cart */
+function recalculateCart(onlyTotal) {
+  var subtotal = 0;
+
+  /* Sum Row Totals */
+  $('.basket-product').each(function() {
+    subtotal += parseFloat($(this).children('.subtotal').text());
+  });
+
+  /* Calculate Totals */
+  var total = subtotal;
+
+  
+  var promoPrice = parseFloat($('.promo-value').text());
+  if (promoPrice) {
+    if (subtotal >= 10) {
+      total -= promoPrice;
+    } else {
+      alert('Order must be more than $10 for Promo Code to apply.');
+      $('.summary-promo').addClass('hide');
+    }
+  }
+
+  /* Update only total display*/
+  if (onlyTotal) {
+    /* Update total display */
+    $('.total-value').fadeOut(fadeTime, function() {
+      $('#basket-total').html(total.toFixed(2));
+      $('.total-value').fadeIn(fadeTime);
+    });
+  } else {
+    /* Update summary display. */
+    $('.final-value').fadeOut(fadeTime, function() {
+      $('#basket-subtotal').html(subtotal.toFixed(2));
+      $('#basket-total').html(total.toFixed(2));
+      if (total == 0) {
+        $('.checkout-cta').fadeOut(fadeTime);
+      } else {
+        $('.checkout-cta').fadeIn(fadeTime);
+      }
+      $('.final-value').fadeIn(fadeTime);
+    });
+  }
+}
+
+/* Update quantity */
+function updateQuantity(quantityInput) {
+  /* Calculate line price */
+  var productRow = $(quantityInput).parent().parent();
+  var price = parseFloat(productRow.children('.price').text());
+  var quantity = $(quantityInput).val();
+  var linePrice = price * quantity;
+
+  /* Update line price display and recalc cart totals */
+  productRow.children('.subtotal').each(function() {
+    $(this).fadeOut(fadeTime, function() {
+      $(this).text(linePrice.toFixed(2));
+      recalculateCart();
+      $(this).fadeIn(fadeTime);
+    });
+  });
+
+  productRow.find('.item-quantity').text(quantity);
+  updateSumItems();
+}
+
+function updateSumItems() {
+  var sumItems = 0;
+  $('.quantity input').each(function() {
+    sumItems += parseInt($(this).val());
+  });
+  $('.total-items').text(sumItems);
+}
+
+/* Remove item from cart */
+function removeItem(removeButton) {
+  /* Remove row from DOM and recalc cart total */
+  var productRow = $(removeButton).parent().parent();
+  productRow.slideUp(fadeTime, function() {
+    productRow.remove();
+    recalculateCart();
+    updateSumItems();
+  });
 }
